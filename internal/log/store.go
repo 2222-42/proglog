5eeabbfb1d6e6b375a4efac1fd142ac45d2e06cb
2222-42 <stephen0617@me.com>
2022-09-06 14:04:53 +0900

    feat(log/store): ストアのコーディング

---
+++ internal/log/store.go
@@ -0,0 +1,100 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var enc = binary.BigEndian
+
+const lenWidth = 8
+
+type store struct {
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		buf:  bufio.NewWriter(f),
+		size: size,
+	}, nil
+}
+
+// Append
+// 与えられたバイトをストアに永続化する
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil { // バッファ付きでライターに書き込むことでシステムコールの数を減らす
+		return 0, 0, err
+	}
+
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	w += lenWidth
+	s.size += uint64(w)
+
+	return uint64(w), pos, nil
+}
+
+// Read
+// 指定されたいいtに格納されているレコードを返す
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil { // バッファがまだディスクにフラッシュされていないレコードに備え
+		return nil, err
+	}
+
+	// レコード全体を読み取るために何バイトを読むかを調べる。
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// ReadAt
+// int64, not uint64
+// ストアのファイルのoffオフセットから、len(p)バイトをpへと読み込む
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+
+	return s.File.ReadAt(p, off)
+}
+
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return err
+	}
+	return s.File.Close()
+}
