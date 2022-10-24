de23663b16d9cc41409d97dd1c59045073330516
2222-42 <stephen0617@me.com>
2022-10-24 09:43:45 +0900

    chore(server_test): 相互TLS認証によるクライアントの認証
    pp.91-93

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -40,7 +40,9 @@ func setupTest(t *testing.T, fn func(config *Config)) (client api.LogClient, cfg
 	require.NoError(t, err)
 
 	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
-		CAFile: config.CAFile,
+		CertFile: config.ClientCertFile,
+		KeyFile:  config.ClientKeyFile,
+		CAFile:   config.CAFile,
 	})
 	require.NoError(t, err)
 
@@ -56,6 +58,7 @@ func setupTest(t *testing.T, fn func(config *Config)) (client api.LogClient, cfg
 		KeyFile:       config.ServerKeyFile,
 		CAFile:        config.CAFile,
 		ServerAddress: l.Addr().String(),
+		Server:        true,
 	})
 	require.NoError(t, err)
 	serverCreds := credentials.NewTLS(serverTLSConfig)
