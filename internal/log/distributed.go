b2a97b91e5e5e76e30b33403e43ac07f7d1f49c3
2222-42 <stephen0617@me.com>
2022-12-29 07:38:16 +0900

    chore(log/distributed): Raft
    pp. 150-152

---
+++ internal/log/distributed.go
@@ -0,0 +1,50 @@
+package log
+
+import (
+	"github.com/hashicorp/raft"
+	"os"
+	"path/filepath"
+)
+
+type DistributedLog struct {
+	config  Config
+	log     *Log
+	raftLog *logStore
+	raft    *raft.Raft
+}
+
+func NewDistributedLog(dataDir string, config Config) (*DistributedLog, error) {
+	l := &DistributedLog{
+		config: config,
+	}
+	if err := l.setupLog(dataDir); err != nil {
+		return nil, err
+	}
+	if err := l.setupRaft(dataDir); err != nil {
+		return nil, err
+	}
+
+	return l, nil
+}
+func (l *DistributedLog) setupLog(dataDir string) error {
+	logDir := filepath.Join(dataDir, "log")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return err
+	}
+
+	var err error
+	l.log, err = NewLog(logDir, l.config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (l *DistributedLog) setupRaft(dataDir string) error {
+	return nil
+}
+
+type logStore struct {
+	*Log
+}
