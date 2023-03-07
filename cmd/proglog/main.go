192aa02f81043b5d461541bf1c8b475aae78ecd2
2222-42 <stephen0617@me.com>
2023-03-07 23:19:55 +0900

    fix(cmd): typo

--- cmd/proglog/main.go
+++ cmd/proglog/main.go
@@ -15,11 +15,11 @@ import (
 func main() {
 	cli := &cli{}
 
-	cmd := &cobra.Command{
-		Use:     "proglog",
-		PreRunE: cli.setupConfig,
-		RunE:    cli.run,
-	}
+	cmd := &cobra.Command{ // <--
+		Use:     "proglog",       // <--
+		PreRunE: cli.setupConfig, // <--
+		RunE:    cli.run,         // <--
+	} // <--
 
 	if err := setupFlags(cmd); err != nil {
 		log.Fatal(err)
@@ -43,25 +43,26 @@ type cfg struct {
 func setupFlags(cmd *cobra.Command) error {
 	hostname, err := os.Hostname()
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 
-	cmd.Flags().String("config-file", "", "Path to config file")
+	cmd.Flags().String("config-file", "", "Path to config file.")
 
 	dataDir := path.Join(os.TempDir(), "proglog")
 	cmd.Flags().String("data-dir",
 		dataDir,
-		"Directory to store log and Raft data",
-	)
-	cmd.Flags().String("node-name", hostname, "Unique server ID")
+		"Directory to store log and Raft data.")
+	cmd.Flags().String("node-name", hostname, "Unique server ID.")
 
 	cmd.Flags().String("bind-addr",
-		"127.0.0.1:84001",
+		"127.0.0.1:8401",
 		"Address to bind Serf on.")
 	cmd.Flags().Int("rpc-port",
 		8400,
 		"Port for RPC clients (and Raft) connections.")
-	cmd.Flags().StringSlice("start-join-addrs", nil, "Serf addresses to join.")
+	cmd.Flags().StringSlice("start-join-addrs",
+		nil,
+		"Serf addresses to join.")
 	cmd.Flags().Bool("bootstrap", false, "Bootstrap the cluster.")
 
 	cmd.Flags().String("acl-model-file", "", "Path to ACL model.")
@@ -69,11 +70,15 @@ func setupFlags(cmd *cobra.Command) error {
 
 	cmd.Flags().String("server-tls-cert-file", "", "Path to server tls cert.")
 	cmd.Flags().String("server-tls-key-file", "", "Path to server tls key.")
-	cmd.Flags().String("server-tls-ca-file", "", "Path to server certificate authority.")
+	cmd.Flags().String("server-tls-ca-file",
+		"",
+		"Path to server certificate authority.")
 
 	cmd.Flags().String("peer-tls-cert-file", "", "Path to peer tls cert.")
 	cmd.Flags().String("peer-tls-key-file", "", "Path to peer tls key.")
-	cmd.Flags().String("peer-tls-ca-file", "", "Path to peer certificate authority.")
+	cmd.Flags().String("peer-tls-ca-file",
+		"",
+		"Path to peer certificate authority.")
 
 	return viper.BindPFlags(cmd.Flags())
 }
@@ -99,7 +104,7 @@ func (c *cli) setupConfig(cmd *cobra.Command, args []string) error {
 	c.cfg.RPCPort = viper.GetInt("rpc-port")
 	c.cfg.StartJoinAddrs = viper.GetStringSlice("start-join-addrs")
 	c.cfg.Bootstrap = viper.GetBool("bootstrap")
-	c.cfg.ACLModelFile = viper.GetString("acl-model-file")
+	c.cfg.ACLModelFile = viper.GetString("acl-mode-file")
 	c.cfg.ACLPolicyFile = viper.GetString("acl-policy-file")
 	c.cfg.ServerTLSConfig.CertFile = viper.GetString("server-tls-cert-file")
 	c.cfg.ServerTLSConfig.KeyFile = viper.GetString("server-tls-key-file")
@@ -133,13 +138,12 @@ func (c *cli) setupConfig(cmd *cobra.Command, args []string) error {
 }
 
 func (c *cli) run(cmd *cobra.Command, args []string) error {
-	ag, err := agent.New(c.cfg.Config)
+	agent, err := agent.New(c.cfg.Config)
 	if err != nil {
 		return err
 	}
-
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
 	<-sigc
-	return ag.Shutdown()
+	return agent.Shutdown()
 }
